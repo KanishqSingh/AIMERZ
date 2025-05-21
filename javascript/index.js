f62ea31a44a8bac5c7c85d051e83223e59be47fa
Kanishq SIngh <singhkanishq06@gmail.com>
2025-05-21 15:58:06 +0530

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,45 @@
+
+// "use strict"
+
+// {let a = 10;
+// const b = 15;}
+
+// console.log(a,b) //reference error : a not defined
+
+// let a = 9 ;
+// a=122;
+// const b = 10;
+// console.log(a,b);
+
+// function show(){
+//     var name = "peter parker"
+// }
+
+let a = 9;
+console.log(a++);
+console.log(++a);
+
+let boolb = true;
+let num = + boolb;
+console.log(num);
+console.log(+ true);
+
+
+let val = '11'
+let res = 22 + val;
+console.log(res);
+
+if (temp < 0) {
+    console.log('freezing');
+    
+    
+} else if(temp >= 0 && temp < 10) {
+    console.log('cold');
+    
+}
+else if(temp >11 && temp < 25) {
+    console.log('cold');
+    
+}
+
+let aaaaa = prompt('enter a number');
