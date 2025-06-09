94252b69dfc86dfe02380974600b9fd999b27e1a
Kanishq SIngh <singhkanishq06@gmail.com>
2025-06-09 22:02:07 +0530

    initial commit

--- javascript/index.js
+++ javascript/index.js
@@ -52,4 +52,66 @@ const display = () =>{
 }
 console.log(display());
 
+//
+function outer() {
+    console.log("outer");
+    function inner() {
+        console.log("inner");
+        
+        
+    }
+    
+    
+}
+outer();
+//
+function outer() {
+    console.log("outer");
+    function inner() {
+        console.log("inner");
+        
+        
+    }
+
+    return inner
+    
+    
+}
+outer()();
+//
+function outer() {
+    console.log("outer");
+    function inner() {
+        console.log("inner");
+        
+        
+    }
+
+    return inner
+    
+    
+}
+
+const aa = outer();
+aa();
+//
+console.log(a1);
+
+const a1 = 9;
+
+show();
+function show() {
+    console.log("hell0");
+    
+    
+}
+
+//closure
+
+
+//lexical environment
+
+
+
+//
 
