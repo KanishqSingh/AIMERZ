7dafe639458edcd8c6c9337e3a9ab33b942f33e8
Kanishq SIngh <singhkanishq06@gmail.com>
2025-07-04 23:06:48 +0530

    initial commit

--- Aimerz_Ebook_JS/Strings/index.js
+++ Aimerz_Ebook_JS/Strings/index.js
@@ -15,6 +15,37 @@ function roundNumber(num) {
 console.log(roundNumber(4.3)); 
 console.log(roundNumber(4.7)); 
 
+//Working with Large Numbers: Formatting and Display
+function formatLargeNumber(number) {
 
+    if(number > 1000 && number < 100000 ){
+        return (number/1000).toFixed(1) + 'k'
+    }else if(number > 100000 && number < 10000000 ){
+        return (number/1000000).toFixed(1) + 'M'
+    }else{
+        return number.toString();
+    }
+    
+}
+console.log(formatLargeNumber(1500));  // Output: "1.5K"
+console.log(formatLargeNumber(3500000)); // Output: "3.5M"
+console.log(formatLargeNumber(950)); // Output: "950"
 
+//
+
+function calculateBulkDiscount(price, quantity, discountThreshold, discountRate) {
+
+    const totalPrice = price * quantity;
+    if(quantity > discountThreshold){
+        const discountedPrice = (totalPrice * (100 - discountRate)) / 100;
+        return discountedPrice;
+    }else{
+        return totalPrice;
+    }
+    
+}
+console.log(calculateBulkDiscount(50, 15, 10, 10)); // Output: 675
+console.log(calculateBulkDiscount(50, 5, 10, 10));  // Output: 250
+console.log(calculateBulkDiscount(100, 20, 15, 20)); // Output: 1600
+ 
   
