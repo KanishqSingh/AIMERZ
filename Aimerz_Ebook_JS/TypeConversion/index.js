f1081b1e0cdc1d2a3ebbe41ce4d165daa607a396
Kanishq SIngh <singhkanishq06@gmail.com>
2025-07-10 22:36:14 +0530

    initial commit

--- Aimerz_Ebook_JS/TypeConversion/index.js
+++ Aimerz_Ebook_JS/TypeConversion/index.js
@@ -14,3 +14,16 @@ function validateAge(ageInput) {
 // Example usage
 let ageInput = "25";  // User input as a string
 validateAge(ageInput);
+
+
+
+ // Convert to string and remove non-numeric characters
+  let cleanedPhoneNumber = String(phoneInput).replace(/D/g, ''); 
+
+  // Ensure the phone number has exactly 10 digits
+  if (cleanedPhoneNumber.length === 10) {
+    console.log("Valid phone number:", cleanedPhoneNumber);  // "5551234567"
+  } else {
+    console.log("Invalid phone number format");
+  }
+
